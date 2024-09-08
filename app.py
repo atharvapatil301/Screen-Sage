3fe559bf06ea7c574ace712a6de20ab40bdd2969
Atharva Patil <95381092+Progpr@users.noreply.github.com>
2024-09-08 12:16:13 +0530

    done trying gpt api

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from openai import OpenAI
 import os
 
-MODEL = "gpt-4o"
-client = OpenAI(api_key = os.emviron.get("OPEN_API_KEY"))
+MODEL = "gpt-4"
+client = OpenAI(api_key = os.environ.get("OPEN_API_KEY"))
 
 #BASIC CHAT
 completion = client.chat.completions.create(
@@ -13,4 +13,30 @@ completion = client.chat.completions.create(
     ]
 )
 
-print("Assistant: " + completion.choices[0].message.content)
+# print("Assistant: " + completion.choices[0].message.content)
+
+# 2  - Image processing with Base64
+import base64
+
+IMAGE_PATH = "yellowsub.png"
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.base64.encode(image_file.read()).decode("utf-8")
+
+ 
+base64_image = encode_image(IMAGE_PATH)
+
+completion = client.chat.completions.create(
+    model = MODEL,
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant which analyses the image to answer user questions about the image"},
+        {"role": "user", "content": [
+            {"type":"text", "text": "What colour is the submarine in the image?"},
+            {"type": "image_url", "image_url": {"url":f"data:image/png;base64,{base64_image}"}}
+        ]}
+
+    ],
+    temperature = 0.0
+)
+print(completion.choices[0].message.content)
